703ee65db32d80a34545377d7ee1b80bffca8e70
nahidul4007 <mdnahidulislam4007@gmail.com>
2026-01-23 01:46:52 +0600

    last update . last module 13.6  . next module 13.7

---
+++ java-script/math.js
@@ -0,0 +1,45 @@
+//  math needed for programming. 
+
+//  variable --> vari + able 
+
+var weight = 38 ;
+var price = 38 ;
+var year = 1999;
+var ballance=2000;
+price=500;
+
+var friend;
+console.log(price);
+console.log(weight);
+console.log(year);
+
+
+
+
+
+
+const Admin ='SPIDER';
+// Admin = 20;        use kora jabe . nah karon value constand
+console.log (Admin)
+
+
+
+
+var isrich = true;
+var isrich1 = false;
+
+console.log ( typeof isrich)
+console.log (isrich1)
+
+
+
+
+
+
+
+
+
+
+
+
+
